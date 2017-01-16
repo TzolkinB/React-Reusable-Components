55673595934b7f5fff8f2ca45bd32c9fbedbdfe2
Kimberly Bell <kimberly.bell@tma1.com>
2017-01-16 15:52:34 -0500

    Start HERE. Section 1 complete

---
+++ source/client.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Portfolio from './components/portfolio';
+
+
+ReactDOM.render(<Portfolio />, document.getElementById('page-top'));
